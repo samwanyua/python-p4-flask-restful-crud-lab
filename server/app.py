571ab7d9764ad60e9c941070181a4c677f9944e3
samwanyua <samexample8@gmail.com>
2024-04-12 22:34:00 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -46,6 +46,30 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        data = request.get_json()
+        if not data or 'is_in_stock' not in data:
+            return make_response(jsonify({"error": "Missing required data"}), 400)
+
+        plant = db.session.get(Plant, id)
+        if plant is None:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        plant.is_in_stock = data['is_in_stock']
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    
+    def delete(self, id):
+        plant = db.session.get(Plant, id)
+        if plant is None:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return '', 204
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
